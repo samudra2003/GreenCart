b8eef7704e5c9051838e4d8d1c0a81301e811700
samudrababai <147082816+samudrababai@users.noreply.github.com>
2025-04-14 17:00:09 +0530

    completed AddAddress & MyOrder page

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -23,11 +23,12 @@ const ProductCard = ({product}) => {
                 </div>
                 <div className="flex items-end justify-between mt-3">
                     <p className="md:text-xl text-base font-medium text-primary">
-                        {currency}${product.offerPrice} {" "} <span className="text-gray-500/60 md:text-sm text-xs line-through">{currency}${product.price}</span>
+                        {currency}{product.offerPrice} {" "} <span className="text-gray-500/60 md:text-sm text-xs line-through">{currency}{product.price}</span>
                     </p>
                     <div  onClick={(e)=>{e.stopPropagation();}} className="text-primary">
                         {  !cartItems[product._id]? (
-                            <button className="flex items-center justify-center gap-1 bg-primary/10 border border-primary/40 md:w-[80px] w-[64px] h-[34px] rounded text-primary-600 font-medium cursor-pointer" onClick={() => addToCart(product._id)} >
+                            <button className="flex items-center justify-center gap-1 bg-primary/10 border border-primary/40 md:w-[80px] w-[64px] h-[34px] rounded text-primary-600 font-medium cursor-pointer"
+                             onClick={() => addToCart(product._id)} >
                                 <img  src={assets.cart_icon} alt="cart_icon" />
                                 Add
                             </button>
