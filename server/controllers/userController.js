15c9411bf1d72823cf590b3b63379763c92e9970
samudrababai <147082816+samudrababai@users.noreply.github.com>
2025-04-15 18:10:56 +0530

    initial backend setup and configuration, completed User model,controllers,route api

---
+++ server/controllers/userController.js
@@ -0,0 +1,85 @@
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+//Register user: /api/user/register
+export const register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    
+     if(!name || !email || !password) {
+      return res.json({ success:false, message: "Missing Details"});
+    }
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.json({ success: false, message: "User already exists" });
+    }
+    const hashedPassword  = await  bcrypt.hash(password, 10);
+    // Create new user
+    const  user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // Generate JWT token
+    const token = jwt.sign({id:user._id},process.env.JWT_SECRET, {expiresIn:'7d'});
+
+    // Set cookie with token
+    res.cookie('token', token, { 
+        httpOnly: true,  //prevent client-side JS from accessing the cookie
+        secure: process.env.NODE_ENV === 'production', //use secure cookies in production
+        sameSite: process.env.NODE_ENV === 'production' ?'none':'strict' , //CSRF protection
+        maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days(cookie expiration time)
+     });
+
+    return res.json({ success: true, user: {email: user.email,name : user.name} });
+
+  } catch (error) {
+    console.error( error.message);
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+//Login user: /api/user/login
+export const login = async (req, res) => {
+  
+    try {
+        const { email, password } = req.body;
+        
+         if(!email || !password) {
+        return res.json({ success:false, message: "Email and Password are required"});
+        }
+        // Check if user exists
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+        return res.json({ success: false, message: "Invalid email or password"});
+        }
+        // Check password
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+        if (!isPasswordValid) {
+          return res.json({ success: false, message: "Invalid email or password" });
+        }
+        // Generate JWT token
+        const token = jwt.sign({id:existingUser._id},process.env.JWT_SECRET, {expiresIn:'7d'});
+    
+        // Set cookie with token
+        res.cookie('token', token, { 
+            httpOnly: true,  //prevent client-side JS from accessing the cookie
+            secure: process.env.NODE_ENV === 'production', //use secure cookies in production
+            sameSite: process.env.NODE_ENV === 'production' ?'none':'strict' , //CSRF protection
+            maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days(cookie expiration time)
+         });
+    
+         return res.json({ success: true, user:{email : existingUser.email,name : existingUser.name} });
+    
+    } catch (error) {
+        console.error( error.message);
+        return res.json({ success: false, message: error.message });
+    }
+
+ }
+
+
+
---
+++ server/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import connectDB from './configs/db.js';
+import 'dotenv/config';
+import userRouter from './routes/userRoute.js';
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+await connectDB();
+
+
+//allow multiple origins
+const  allowedOrigins = [
+    'http://localhost:5173', // Development URL
+];
+
+
+// Middleware configuration
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: allowedOrigins, // Replace with your frontend URL
+    credentials: true,
+}));
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('API is working...');
+}
+);
+
+app.use('/api/user', userRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+}   
+);
