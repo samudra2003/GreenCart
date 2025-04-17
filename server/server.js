9b2b85e976c2c035501f9240597cf42c9dc7dd43
samudrababai <147082816+samudrababai@users.noreply.github.com>
2025-04-17 17:44:05 +0530

    completed Order , Cart,Address Controller,Route,model api

--- server/server.js
+++ server/server.js
@@ -7,6 +7,9 @@ import userRouter from './routes/userRoute.js';
 import sellerRouter from './routes/sellerRoute.js';
 import connectToCloudinary from './configs/cloudinary.js';
 import productRouter from './routes/productRoute.js';
+import cartRouter from './routes/cartRoute.js';
+import addressRouter from './routes/addressRoute.js';
+import orderRouter from './routes/orderRoute.js';
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -40,6 +43,9 @@ app.get('/', (req, res) => {
 app.use('/api/user', userRouter);
 app.use('/api/seller', sellerRouter);
 app.use('/api/product', productRouter);
+app.use('/api/cart', cartRouter);
+app.use('/api/address', addressRouter);
+app.use('/api/order', orderRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
